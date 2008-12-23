5fcce012e119965f0751f5e0a470a26e9fd5dc48
Andreas Gohr <gohr@cosmocode.de>
2008-12-23 12:24:53 +0100

    Added admin interface to view the log
    darcs-hash:20081223112453-6e07b-700506a1cc29a313cd7987a9585ace601e627831.gz

---
+++ admin.php
@@ -0,0 +1,174 @@
+<?php
+
+// must be run within Dokuwiki
+if(!defined('DOKU_INC')) die();
+
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+require_once(DOKU_PLUGIN.'admin.php');
+
+
+class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
+
+    /**
+     * return some info
+     */
+    function getInfo(){
+        return confToHash(dirname(__FILE__).'/info.txt');
+    }
+
+    /**
+     * Access for managers allowed
+     */
+    function forAdminOnly(){
+        return false;
+    }
+
+    /**
+     * return sort order for position in admin menu
+     */
+    function getMenuSort() {
+        return 141;
+    }
+
+    /**
+     * handle user request
+     */
+    function handle() {
+    }
+
+    /**
+     * output appropriate html
+     */
+    function html() {
+        global $conf;
+        global $lang;
+        $go  = (int) $_REQUEST['time'];
+        if(!$go) $go = time()+60*60; //one hour in the future to trick pagination
+        $min = $go-(7*24*60*60);
+        $max = $go;
+
+        echo $this->plugin_locale_xhtml('intro');
+
+        echo '<p>'.$this->getLang('range').' '.strftime($conf['dformat'],$min).
+             ' - '.strftime($conf['dformat'],$max).'</p>';
+
+
+        echo '<table class="inline loglog">';
+        echo '<tr>';
+        echo '<th>'.$this->getLang('date').'</th>';
+        echo '<th>'.$this->getLang('ip').'</th>';
+        echo '<th>'.$lang['user'].'</th>';
+        echo '<th>'.$this->getLang('action').'</th>';
+        echo '</tr>';
+
+        $lines = $this->_readlines($min,$max);
+        $lines = array_reverse($lines);
+
+        foreach($lines as $line){
+            list($dt,$junk,$ip,$user,$msg) = explode("\t",$line,5);
+            if($dt < $min) continue;
+            if($dt > $max) continue;
+            if(!$user)     continue;
+
+            if($msg == 'logged off'){
+                $msg = $this->getLang('off');
+                $class = 'off';
+            }elseif($msg == 'logged in permanently'){
+                $msg = $this->getLang('in');
+                $class = 'perm';
+            }elseif($msg == 'logged in temporarily'){
+                $msg = $this->getLang('tin');
+                $class = 'temp';
+            }else{
+                $msg = '****'.hsc($msg).'****';
+                $class = 'error';
+            }
+
+            echo '<tr>';
+            echo '<td>'.strftime($conf['dformat'],$dt).'</td>';
+            echo '<td>'.hsc($ip).'</td>';
+            echo '<td>'.hsc($user).'</td>';
+            echo '<td><span class="loglog_'.$class.'">'.$msg.'</span></td>';
+            echo '</tr>';
+        }
+
+        echo '</table>';
+
+        echo '<div class="pagenav">';
+        if($max < time()-(7*24*60*60)){
+        echo '<div class="pagenav-prev">';
+        echo html_btn('newer',$ID,"p",array('do'=>'admin','page'=>'loglog','time'=>$max+(7*24*60*60)));
+        echo '</div>';
+        }
+
+        echo '<div class="pagenav-next">';
+        echo html_btn('older',$ID,"n",array('do'=>'admin','page'=>'loglog','time'=>$min));
+        echo '</div>';
+        echo '</div>';
+
+    }
+
+    /**
+     * Read loglines backward
+     *
+     * @param int $min - start time (in seconds)
+     */
+    function _readlines($min,$max){
+        global $conf;
+        $file = $conf['cachedir'].'/loglog.log';
+
+
+        $data  = array();
+        $lines = array();
+        $chunk_size = 8192;
+
+        if (!@file_exists($file)) return $data;
+        $fp = fopen($file, 'rb');
+        if ($fp===false) return $data;
+
+        //seek to end
+        fseek($fp, 0, SEEK_END);
+        $pos = ftell($fp);
+        $chunk = '';
+
+        while($pos){
+
+            // how much to read? Set pointer
+            if($pos > $chunk_size){
+                $pos -= $chunk_size;
+                $read = $chunk_size;
+            }else{
+                $read = $pos;
+                $pos  = 0;
+            }
+            fseek($fp,$pos);
+
+            $tmp = fread($fp,$read);
+            if($tmp === false) break;
+            $chunk = $tmp.$chunk;
+
+            // now split the chunk
+            $cparts = explode("\n",$chunk);
+
+            // keep the first part in chunk (may be incomplete)
+            if($pos) $chunk = array_shift($cparts);
+
+            // no more parts available, read on
+            if(!count($cparts)) continue;
+
+            // get date of first line:
+            list($cdate) = explode("\t",$cparts[0]);
+
+            if($cdate > $max) continue; // haven't reached wanted area, yet
+
+            // put the new lines on the stack
+            $lines = array_merge($cparts,$lines);
+
+            if($cdate < $min) break; // we have enough
+        }
+        fclose($fp);
+
+        return $lines;
+    }
+}
+//Setup VIM: ex: et ts=4 enc=utf-8 :
---
+++ lang/en/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['menu'] = 'Weekly User Logins/Logouts';
+
+$lang['date']   = 'Date';
+$lang['ip']     = 'IP Address';
+$lang['action'] = 'Action';
+$lang['range']  = 'Displayed date range:';
+
+$lang['off'] = 'logged off';
+$lang['in']  = 'logged in permanently';
+$lang['tin'] = 'logged in temporarily';
