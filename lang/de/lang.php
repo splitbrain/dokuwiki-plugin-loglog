354301166b70a6a834c36debd760534d6a3530c6
Andreas Gohr <gohr@cosmocode.de>
2008-12-23 12:30:24 +0100

    German translation added
    darcs-hash:20081223113024-6e07b-e44427c230204aa2687412f6d27a04e0ddf32322.gz

---
+++ lang/de/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['menu'] = 'Wöchentlichen Benutzer An- und Abmeldungen';
+
+$lang['date']   = 'Datum';
+$lang['ip']     = 'IP Adresse';
+$lang['action'] = 'Aktion';
+$lang['range']  = 'Angezeigter Zeitraum:';
+
+$lang['off'] = 'abgemeldet';
+$lang['in']  = 'dauerhaft angemeldet';
+$lang['tin'] = 'temporär angemeldet';
