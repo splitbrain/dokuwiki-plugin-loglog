aaa4a0ee9e57a4c0efea1e16abc920c9572658aa
Jean Christophe Charvy <jc.charvy@gmail.com>
2010-08-01 21:04:33 +0200

    French translation added

---
+++ lang/fr/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+$lang['menu'] = 'Journal hebdomadaire des Connexions/Déconnexions Utilisateurs';
+
+$lang['date']   = 'Date';
+$lang['ip']     = 'Adresse IP';
+$lang['action'] = 'Action';
+$lang['range']  = 'Période affichée :';
+
+$lang['off'] = 'déconnecté';
+$lang['in']  = 'connecté de façon permanente';
+$lang['tin'] = 'connecté temporairement';
