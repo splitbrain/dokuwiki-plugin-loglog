4e568b30d1ce723dda1ae841447f49e5aca74fc5
Myeongjin <aranet100@gmail.com>
2013-08-14 16:05:05 +0200

    translation update

---
+++ lang/ko/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Myeongjin <aranet100@gmail.com>
+ */
+$lang['menu']                  = '주간 사용자 로그인/로그아웃';
+$lang['date']                  = '날짜';
+$lang['ip']                    = 'IP 주소';
+$lang['action']                = '행동';
+$lang['range']                 = '보여줄 날짜 범위:';
+$lang['off']                   = '로그아웃됨';
+$lang['in']                    = '영구적으로 로그인됨';
+$lang['tin']                   = '일시적으로 로그인됨';
+$lang['fail']                  = '실패한 로그인 시도';
