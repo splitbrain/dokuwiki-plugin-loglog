58f5b037187afa2992cffd953c78f2d9df148008
Hideaki SAWADA <chuno@live.jp>
2013-09-11 17:40:19 +0200

    translation update

---
+++ lang/ja/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+$lang['menu']                  = '週間ユーザーログイン／ログアウト';
+$lang['date']                  = '日付';
+$lang['ip']                    = 'IPアドレス';
+$lang['action']                = '動作';
+$lang['range']                 = '表示中の日付範囲：';
+$lang['off']                   = 'ログオフ';
+$lang['in']                    = '恒久的なログイン';
+$lang['tin']                   = '一時的なログイン';
+$lang['fail']                  = 'ログイン失敗';
