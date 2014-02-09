5ecc451e7ac9bf885ad4a476c8c32c7afcdd602f
Robert Bogenschneider <bogi@uea.org>
2014-02-09 16:15:25 +0100

    translation update

---
+++ lang/eo/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['menu']                  = 'Semajnaj en/elsalutoj de uzantoj';
+$lang['date']                  = 'Dato';
+$lang['ip']                    = 'IP-adreso';
+$lang['action']                = 'Agado';
+$lang['range']                 = 'Montrata dato-periodo:';
+$lang['off']                   = 'elsalutinta';
+$lang['in']                    = 'ensalutinta konstante';
+$lang['tin']                   = 'ensalutinta portempe';
+$lang['fail']                  = 'malsukcesa provo ensaluti';
