1e7b94ce597c051fb1c7b73a233b5eb245d197f5
Aleksandr Selivanov <alexgearbox@yandex.ru>
2014-03-17 16:41:35 +0100

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,13 +1,16 @@
 <?php
 
-$lang['menu'] = 'Журнал авторизации пользователей';
-
-$lang['date']   = 'Дата';
-$lang['ip']     = 'IP-адрес';
-$lang['action'] = 'Действие';
-$lang['range']  = 'Отображаемый период:';
-
-$lang['off']    = 'Выйти';
-$lang['in']     = 'Постоянный вход в систему';
-$lang['tin']    = 'Временный вход в систему';
-$lang['fail']   = 'Неудачная попытка входа';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
+ */
+$lang['menu']                  = 'Журнал авторизации пользователей';
+$lang['date']                  = 'Дата';
+$lang['ip']                    = 'IP-адрес';
+$lang['action']                = 'Действие';
+$lang['range']                 = 'Отображаемый период:';
+$lang['off']                   = 'Выход';
+$lang['in']                    = 'Постоянный вход в систему';
+$lang['tin']                   = 'Временный вход в систему';
+$lang['fail']                  = 'Неудачная попытка входа';
