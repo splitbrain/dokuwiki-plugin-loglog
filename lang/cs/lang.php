bc5f31fb85a00a4df6258f7ec45c275d18b4c927
Jaroslav Lichtblau <jlichtblau@seznam.cz>
2014-09-20 11:10:12 +0200

    translation update

---
+++ lang/cs/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
+ */
+$lang['menu']                  = 'Týdenní uživatelská přihlášení/odhlášení';
+$lang['date']                  = 'Datum';
+$lang['ip']                    = 'IP adresa';
+$lang['action']                = 'Akce';
+$lang['range']                 = 'Zobrazené časové období:';
+$lang['off']                   = 'odhlášení';
+$lang['in']                    = 'trvalé přihlášení';
+$lang['tin']                   = 'dočasné přihlášení';
+$lang['fail']                  = 'chybné přihlášení';
