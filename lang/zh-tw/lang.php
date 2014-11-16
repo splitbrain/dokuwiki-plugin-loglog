a0ae37731bce9ceaf9350c25d123a0026c39a3ad
HL <tohelong@gmail.com>
2014-11-16 13:50:09 +0100

    translation update

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author HL <tohelong@gmail.com>
+ */
+$lang['date']                  = '日期';
+$lang['ip']                    = 'IP';
+$lang['action']                = '動作';
+$lang['range']                 = '日期範圍';
+$lang['off']                   = '登出';
+$lang['in']                    = '記住登錄';
+$lang['tin']                   = '登錄';
+$lang['fail']                  = '登錄失敗';
