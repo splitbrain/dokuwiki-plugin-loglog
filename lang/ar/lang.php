b089fca321642c2d053f2de37bb17762b4ddfc69
Ali Almlfy <Almlfy@hotmail.com>
2014-12-07 02:25:37 +0100

    translation update

---
+++ lang/ar/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Ali Almlfy <Almlfy@hotmail.com>
+ */
+$lang['menu']                  = 'سجل الدخول و الخروج الاسبوعي للمستخدم ';
+$lang['date']                  = 'التاريخ';
+$lang['ip']                    = 'عنوان الايبي';
+$lang['action']                = 'نفذ';
+$lang['range']                 = 'اظهر سجل التاريخ';
+$lang['off']                   = 'سجل خروج';
+$lang['in']                    = 'سجل دخول متواصل ';
+$lang['tin']                   = 'سجل دخول موقته';
+$lang['fail']                  = 'محاولت دخول فاشلة';
