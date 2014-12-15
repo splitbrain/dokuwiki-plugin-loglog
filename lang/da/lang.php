1a75558900b8af8c89e2781096bd001252c7371b
Søren Birk <soer9648@eucl.dk>
2014-12-15 13:45:13 +0100

    translation update

---
+++ lang/da/lang.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Søren Birk <soer9648@eucl.dk>
+ */
+$lang['menu']                  = 'Ugentlige Brugerlogin/log ud';
+$lang['date']                  = 'Dato';
+$lang['ip']                    = 'IP-Adresse';
+$lang['action']                = 'Handling';
+$lang['range']                 = 'Vist datorækkevidde';
+$lang['off']                   = 'logget ud';
+$lang['in']                    = 'permanent logget ind';
+$lang['tin']                   = 'midlertidigt logget ind';
+$lang['fail']                  = 'fejlet login-forsøg';
