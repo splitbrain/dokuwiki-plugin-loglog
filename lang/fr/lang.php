b07fc118b1f96f79fcba6f6e5637326698d7f50b
Nicolas Friedli <nicolas@theologique.ch>
2015-04-13 23:45:35 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author NicolasFriedli <nicolas@theologique.ch>
+ * @author Nicolas Friedli <nicolas@theologique.ch>
  */
 $lang['menu']                  = 'Journal hebdomadaire des Connexions/Déconnexions Utilisateurs';
 $lang['date']                  = 'Date';
@@ -11,6 +12,7 @@ $lang['ip']                    = 'Adresse IP';
 $lang['action']                = 'Action';
 $lang['range']                 = 'Période affichée :';
 $lang['off']                   = 'déconnecté';
+$lang['autologoff']            = 'a été connecté automatiquement';
 $lang['in']                    = 'connecté de façon permanente';
 $lang['tin']                   = 'connecté temporairement';
 $lang['fail']                  = 'connextion ratée';
