c419802dfcd1c9cd4d004452f0d253e6c423a07e
Satoshi Sahara <sahara.satoshi@gmail.com>
2015-07-02 23:54:28 +0900

    update Japanese lang file

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Hideaki SAWADA <chuno@live.jp>
+ * @author Satoshi Sahara <sahara.satoshi@gmail.com>
  */
 $lang['menu']                  = '週間ユーザーログイン／ログアウト';
 $lang['date']                  = '日付';
@@ -11,6 +12,7 @@ $lang['ip']                    = 'IPアドレス';
 $lang['action']                = '動作';
 $lang['range']                 = '表示中の日付範囲：';
 $lang['off']                   = 'ログオフ';
+$lang['autologoff']            = '自動ログオフの検出';
 $lang['in']                    = '恒久的なログイン';
 $lang['tin']                   = '一時的なログイン';
 $lang['fail']                  = 'ログイン失敗';
