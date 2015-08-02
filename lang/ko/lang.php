e83768d1be9e9918ff6a863bc9220d9548453f2b
Myeongjin <aranet100@gmail.com>
2015-08-02 14:45:34 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -11,6 +11,7 @@ $lang['ip']                    = 'IP 주소';
 $lang['action']                = '행동';
 $lang['range']                 = '보여줄 날짜 범위:';
 $lang['off']                   = '로그아웃됨';
+$lang['autologoff']            = '사용자가 자동으로 로그아웃되었습니다';
 $lang['in']                    = '영구적으로 로그인됨';
 $lang['tin']                   = '일시적으로 로그인됨';
 $lang['fail']                  = '실패한 로그인 시도';
