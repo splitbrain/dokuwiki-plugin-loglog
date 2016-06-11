a42bf95537d1312578f22612d7d50d51cb422e1f
Davor Turkalj <turki.bsc@gmail.com>
2016-06-11 17:45:32 +0200

    translation update

---
+++ lang/hr/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['menu']                  = 'Tjedne prijave/odjave korisnika';
+$lang['date']                  = 'Datum';
+$lang['ip']                    = 'IP adresa';
+$lang['action']                = 'Aktivnost';
+$lang['range']                 = 'Prikazani opseg datuma:';
+$lang['off']                   = 'odjavljen';
+$lang['autologoff']            = 'bio je odjavljen od strane sustava';
+$lang['in']                    = 'prijavljen trajno';
+$lang['tin']                   = 'prijavljen trenutno';
+$lang['fail']                  = 'neuspio pokušaj prijave';
