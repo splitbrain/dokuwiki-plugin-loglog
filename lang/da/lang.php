fa09c975ea02f8e181558dc295e8533431e07fe0
Jacob Palm <mail@jacobpalm.dk>
2016-06-28 15:15:32 +0200

    translation update

--- lang/da/lang.php
+++ lang/da/lang.php
@@ -2,8 +2,9 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Søren Birk <soer9648@eucl.dk>
+ * @author Jacob Palm <mail@jacobpalm.dk>
  */
 $lang['menu']                  = 'Ugentlige Brugerlogin/log ud';
 $lang['date']                  = 'Dato';
@@ -11,6 +12,7 @@ $lang['ip']                    = 'IP-Adresse';
 $lang['action']                = 'Handling';
 $lang['range']                 = 'Vist datorækkevidde';
 $lang['off']                   = 'logget ud';
+$lang['autologoff']            = 'er blevet logget ud automatisk';
 $lang['in']                    = 'permanent logget ind';
 $lang['tin']                   = 'midlertidigt logget ind';
 $lang['fail']                  = 'fejlet login-forsøg';
