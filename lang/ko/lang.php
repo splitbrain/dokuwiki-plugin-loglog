9317374427ca1cad19a7b7f03e1973b05808387a
Myeongjin <aranet100@gmail.com>
2016-07-27 12:57:01 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -2,14 +2,14 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Myeongjin <aranet100@gmail.com>
  */
 $lang['menu']                  = '주간 사용자 로그인/로그아웃';
 $lang['date']                  = '날짜';
 $lang['ip']                    = 'IP 주소';
-$lang['action']                = '행동';
-$lang['range']                 = '보여줄 날짜 범위:';
+$lang['action']                = '동작';
+$lang['range']                 = '표시할 날짜 범위:';
 $lang['off']                   = '로그아웃됨';
 $lang['autologoff']            = '사용자가 자동으로 로그아웃되었습니다';
 $lang['in']                    = '영구적으로 로그인됨';
