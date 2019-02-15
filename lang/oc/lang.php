1dc0f42a6ef14815748554dfced5d45e5b26f422
En Matt <heartattack@free.fr>
2019-02-15 15:25:05 +0100

    translation update

---
+++ lang/oc/lang.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author En Matt <heartattack@free.fr>
+ */
+$lang['menu']                  = 'Connexions/Desconnexions dels utilizaires per setmana';
+$lang['date']                  = 'Data';
+$lang['ip']                    = 'Adreça IP';
+$lang['action']                = 'Accion';
+$lang['range']                 = 'Mostrar lo periòde :';
+$lang['off']                   = 'desconnectat';
+$lang['autologoff']            = 'es estat desconnectat automaticament';
+$lang['in']                    = 'connectat d\'un biais permanent';
+$lang['tin']                   = 'connectat temporàriament';
+$lang['fail']                  = 'ensag mancat de connexion';
