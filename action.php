c48b316a8265a987b571e715a447de3b7cc66b3d
Andreas Gohr <andi@splitbrain.org>
2020-06-08 09:21:31 +0200

    style cleanup

--- action.php
+++ action.php
@@ -6,20 +6,12 @@
  * @author     Andreas Gohr <gohr@cosmocode.de>
  */
 
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
+class action_plugin_loglog extends DokuWiki_Action_Plugin
+{
 
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC . 'lib/plugins/');
-require_once(DOKU_PLUGIN . 'action.php');
-
-class action_plugin_loglog extends DokuWiki_Action_Plugin {
-
-    var $islogin = false;
-
-    /**
-     * register the eventhandlers
-     */
-    function register(Doku_Event_Handler $controller) {
+    /** @inheritDoc */
+    function register(Doku_Event_Handler $controller)
+    {
         $controller->register_hook(
             'ACTION_ACT_PREPROCESS',
             'BEFORE',
@@ -53,15 +45,16 @@ class action_plugin_loglog extends DokuWiki_Action_Plugin {
      * @param $msg
      * @param null|string $user
      */
-    protected function _log($msg, $user = null) {
+    protected function logAccess($msg, $user = null)
+    {
         global $conf;
         global $INPUT;
 
-        if(is_null($user)) $user = $INPUT->server->str('REMOTE_USER');
-        if(!$user) $user = $_REQUEST['u'];
-        if(!$user) return;
+        if (is_null($user)) $user = $INPUT->server->str('REMOTE_USER');
+        if (!$user) $user = $_REQUEST['u'];
+        if (!$user) return;
 
-        $t   = time();
+        $t = time();
         $ip = clientIP(true);
         $log = $t . "\t" . strftime($conf['dformat'], $t) . "\t" . $ip . "\t" . $user . "\t" . $msg;
         io_saveFile($conf['cachedir'] . '/loglog.log', "$log\n", true);
@@ -73,19 +66,20 @@ class action_plugin_loglog extends DokuWiki_Action_Plugin {
      * @param Doku_Event $event
      * @param mixed $param data passed to the event handler
      */
-    public function handle_custom(Doku_Event $event, $param) {
-        if(isset($event->data['message'])) {
+    public function handle_custom(Doku_Event $event, $param)
+    {
+        if (isset($event->data['message'])) {
             $log = $event->data['message'];
         } else {
             return;
         }
-        if(isset($event->data['user'])) {
+        if (isset($event->data['user'])) {
             $user = $event->data['user'];
         } else {
             $user = null;
         }
 
-        $this->_log($log, $user);
+        $this->logAccess($log, $user);
     }
 
     /**
@@ -94,8 +88,9 @@ class action_plugin_loglog extends DokuWiki_Action_Plugin {
      * @param Doku_Event $event
      * @param mixed $param data passed to the event handler
      */
-    public function handle_autologout(Doku_Event $event, $param) {
-        $this->_log('has been automatically logged off');
+    public function handle_autologout(Doku_Event $event, $param)
+    {
+        $this->logAccess('has been automatically logged off');
     }
 
     /**
@@ -104,18 +99,19 @@ class action_plugin_loglog extends DokuWiki_Action_Plugin {
      * @param Doku_Event $event
      * @param mixed $param data passed to the event handler
      */
-    public function handle_before(Doku_Event $event, $param) {
+    public function handle_before(Doku_Event $event, $param)
+    {
         $act = act_clean($event->data);
-        if($act == 'logout') {
-            $this->_log('logged off');
-        } elseif(!empty($_SERVER['REMOTE_USER']) && $act == 'login') {
-            if(isset($_REQUEST['r'])) {
-                $this->_log('logged in permanently');
+        if ($act == 'logout') {
+            $this->logAccess('logged off');
+        } elseif (!empty($_SERVER['REMOTE_USER']) && $act == 'login') {
+            if (isset($_REQUEST['r'])) {
+                $this->logAccess('logged in permanently');
             } else {
-                $this->_log('logged in temporarily');
+                $this->logAccess('logged in temporarily');
             }
-        } elseif($_REQUEST['u'] && empty($_REQUEST['http_credentials']) && empty($_SERVER['REMOTE_USER'])) {
-            $this->_log('failed login attempt');
+        } elseif ($_REQUEST['u'] && empty($_REQUEST['http_credentials']) && empty($_SERVER['REMOTE_USER'])) {
+            $this->logAccess('failed login attempt');
         }
     }
 }
--- admin.php
+++ admin.php
@@ -1,92 +1,76 @@
 <?php
 
-// must be run within Dokuwiki
-if(!defined('DOKU_INC')) die();
+class admin_plugin_loglog extends DokuWiki_Admin_Plugin
+{
 
-if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-require_once(DOKU_PLUGIN.'admin.php');
-
-
-class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
-
-    /**
-     * Access for managers allowed
-     */
-    function forAdminOnly(){
+    /** @inheritDoc */
+    function forAdminOnly()
+    {
         return false;
     }
 
-    /**
-     * return sort order for position in admin menu
-     */
-    function getMenuSort() {
+    /** @inheritDoc */
+    function getMenuSort()
+    {
         return 141;
     }
 
-    /**
-     * handle user request
-     */
-    function handle() {
-    }
-
-    /**
-     * output appropriate html
-     */
-    function html() {
+    /** @inheritDoc */
+    function html()
+    {
         global $ID, $conf, $lang;
         $now = time();
-        $go  = isset($_REQUEST['time']) ? intval($_REQUEST['time']) : $now;
-        $min = $go-(7*24*60*60);
+        $go = isset($_REQUEST['time']) ? intval($_REQUEST['time']) : $now;
+        $min = $go - (7 * 24 * 60 * 60);
         $max = $go;
 
         echo $this->locale_xhtml('intro');
 
-        echo '<p>'.$this->getLang('range').' '.strftime($conf['dformat'],$min).
-             ' - '.strftime($conf['dformat'],$max).'</p>';
-
+        echo '<p>' . $this->getLang('range') . ' ' . strftime($conf['dformat'], $min) .
+            ' - ' . strftime($conf['dformat'], $max) . '</p>';
 
         echo '<table class="inline loglog">';
         echo '<tr>';
-        echo '<th>'.$this->getLang('date').'</th>';
-        echo '<th>'.$this->getLang('ip').'</th>';
-        echo '<th>'.$lang['user'].'</th>';
-        echo '<th>'.$this->getLang('action').'</th>';
+        echo '<th>' . $this->getLang('date') . '</th>';
+        echo '<th>' . $this->getLang('ip') . '</th>';
+        echo '<th>' . $lang['user'] . '</th>';
+        echo '<th>' . $this->getLang('action') . '</th>';
         echo '</tr>';
 
-        $lines = $this->_readlines($min,$max);
+        $lines = $this->readLines($min, $max);
         $lines = array_reverse($lines);
 
-        foreach($lines as $line){
+        foreach ($lines as $line) {
             if (empty($line)) continue; // Filter empty lines
-            list($dt,$junk,$ip,$user,$msg) = explode("\t",$line,5);
-            if($dt < $min) continue;
-            if($dt > $max) continue;
-            if(!$user)     continue;
+            list($dt, $junk, $ip, $user, $msg) = explode("\t", $line, 5);
+            if ($dt < $min) continue;
+            if ($dt > $max) continue;
+            if (!$user) continue;
 
-            if($msg == 'logged off'){
+            if ($msg == 'logged off') {
                 $msg = $this->getLang('off');
                 $class = 'off';
-            }elseif($msg == 'logged in permanently'){
+            } elseif ($msg == 'logged in permanently') {
                 $msg = $this->getLang('in');
                 $class = 'perm';
-            }elseif($msg == 'logged in temporarily'){
+            } elseif ($msg == 'logged in temporarily') {
                 $msg = $this->getLang('tin');
                 $class = 'temp';
-            }elseif($msg == 'failed login attempt'){
+            } elseif ($msg == 'failed login attempt') {
                 $msg = $this->getLang('fail');
                 $class = 'fail';
-            }elseif($msg == 'has been automatically logged off') {
+            } elseif ($msg == 'has been automatically logged off') {
                 $msg = $this->getLang('autologoff');
                 $class = 'off';
-            }else{
+            } else {
                 $msg = hsc($msg);
-                if(strpos($msg, 'logged off') !== false) {
+                if (strpos($msg, 'logged off') !== false) {
                     $class = 'off';
-                } elseif(strpos($msg, 'logged in permanently') !== false) {
+                } elseif (strpos($msg, 'logged in permanently') !== false) {
                     $class = 'perm';
-                } elseif(strpos($msg, 'logged in') !== false) {
+                } elseif (strpos($msg, 'logged in') !== false) {
                     $class = 'temp';
-                } elseif(strpos($msg, 'failed') !== false) {
+                } elseif (strpos($msg, 'failed') !== false) {
                     $class = 'fail';
                 } else {
                     $class = 'unknown';
@@ -94,29 +78,29 @@ class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
             }
 
             echo '<tr>';
-            echo '<td>'.strftime($conf['dformat'],$dt).'</td>';
-            echo '<td>'.hsc($ip).'</td>';
-            echo '<td>'.hsc($user).'</td>';
-            echo '<td><span class="loglog_'.$class.'">'.$msg.'</span></td>';
+            echo '<td>' . strftime($conf['dformat'], $dt) . '</td>';
+            echo '<td>' . hsc($ip) . '</td>';
+            echo '<td>' . hsc($user) . '</td>';
+            echo '<td><span class="loglog_' . $class . '">' . $msg . '</span></td>';
             echo '</tr>';
         }
 
         echo '</table>';
 
         echo '<div class="pagenav">';
-        if($now - $go > 60*60*5){
-            $next = $max+(7*24*60*60);
-            if($now - $next  < 60*60*5) {
+        if ($now - $go > 60 * 60 * 5) {
+            $next = $max + (7 * 24 * 60 * 60);
+            if ($now - $next < 60 * 60 * 5) {
                 $next = $now;
             }
 
             echo '<div class="pagenav-prev">';
-            echo html_btn('newer',$ID,"p",array('do'=>'admin','page'=>'loglog','time'=>$next));
+            echo html_btn('newer', $ID, "p", array('do' => 'admin', 'page' => 'loglog', 'time' => $next));
             echo '</div>';
         }
 
         echo '<div class="pagenav-next">';
-        echo html_btn('older',$ID,"n",array('do'=>'admin','page'=>'loglog','time'=>$min));
+        echo html_btn('older', $ID, "n", array('do' => 'admin', 'page' => 'loglog', 'time' => $min));
         echo '</div>';
         echo '</div>';
 
@@ -125,60 +109,62 @@ class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
     /**
      * Read loglines backward
      *
-     * @param int $min - start time (in seconds)
+     * @param int $min start time (in seconds)
+     * @param int $max end time (in seconds)
+     * @return array
      */
-    function _readlines($min,$max){
+    function readLines($min, $max)
+    {
         global $conf;
-        $file = $conf['cachedir'].'/loglog.log';
+        $file = $conf['cachedir'] . '/loglog.log';
 
-
-        $data  = array();
+        $data = array();
         $lines = array();
         $chunk_size = 8192;
 
         if (!@file_exists($file)) return $data;
         $fp = fopen($file, 'rb');
-        if ($fp===false) return $data;
+        if ($fp === false) return $data;
 
         //seek to end
         fseek($fp, 0, SEEK_END);
         $pos = ftell($fp);
         $chunk = '';
 
-        while($pos){
+        while ($pos) {
 
             // how much to read? Set pointer
-            if($pos > $chunk_size){
+            if ($pos > $chunk_size) {
                 $pos -= $chunk_size;
                 $read = $chunk_size;
-            }else{
+            } else {
                 $read = $pos;
-                $pos  = 0;
+                $pos = 0;
             }
-            fseek($fp,$pos);
+            fseek($fp, $pos);
 
-            $tmp = fread($fp,$read);
-            if($tmp === false) break;
-            $chunk = $tmp.$chunk;
+            $tmp = fread($fp, $read);
+            if ($tmp === false) break;
+            $chunk = $tmp . $chunk;
 
             // now split the chunk
-            $cparts = explode("\n",$chunk);
+            $cparts = explode("\n", $chunk);
 
             // keep the first part in chunk (may be incomplete)
-            if($pos) $chunk = array_shift($cparts);
+            if ($pos) $chunk = array_shift($cparts);
 
             // no more parts available, read on
-            if(!count($cparts)) continue;
+            if (!count($cparts)) continue;
 
             // get date of first line:
-            list($cdate) = explode("\t",$cparts[0]);
+            list($cdate) = explode("\t", $cparts[0]);
 
-            if($cdate > $max) continue; // haven't reached wanted area, yet
+            if ($cdate > $max) continue; // haven't reached wanted area, yet
 
             // put the new lines on the stack
-            $lines = array_merge($cparts,$lines);
+            $lines = array_merge($cparts, $lines);
 
-            if($cdate < $min) break; // we have enough
+            if ($cdate < $min) break; // we have enough
         }
         fclose($fp);
 
