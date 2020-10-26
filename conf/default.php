ff4cde42e7cc988d93000a705c70feb72e180089
Anna Dabrowska <dabrowska@cosmocode.de>
2020-10-26 17:45:26 +0100

    Add some default values to alert config

--- conf/default.php
+++ conf/default.php
@@ -1,9 +1,9 @@
 <?php
 
-$conf['login_success_max'] = '';
-$conf['login_success_interval'] = '';
+$conf['login_success_max'] = '50';
+$conf['login_success_interval'] = '1';
 $conf['login_success_email'] = '';
-$conf['login_failed_max'] = '';
-$conf['login_failed_interval'] = '';
+$conf['login_failed_max'] = '10';
+$conf['login_failed_interval'] = '1';
 $conf['login_failed_email'] = '';
 $conf['report_email'] = '';
