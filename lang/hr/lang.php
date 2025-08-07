0ba7b78295d3020d55bc5e486b70a00c92e10c0e
http://Boyarka-Inform.com/ <brittconlon@facebook.com>
2025-08-07 19:05:49 +0200

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -3,15 +3,56 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author http://Boyarka-Inform.com/ <brittconlon@facebook.com>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['menu']                  = 'Tjedne prijave/odjave korisnika';
-$lang['date']                  = 'Datum';
-$lang['ip']                    = 'IP adresa';
-$lang['action']                = 'Aktivnost';
-$lang['range']                 = 'Prikazani opseg datuma:';
-$lang['off']                   = 'odjavljen';
-$lang['autologoff']            = 'bio je odjavljen od strane sustava';
-$lang['in']                    = 'prijavljen trajno';
-$lang['tin']                   = 'prijavljen trenutno';
-$lang['fail']                  = 'neuspio pokušaj prijave';
+$lang['menu']                  = 'Britt';
+$lang['date']                  = 'I\'m truly enjoying thee design and layout of your blog. It\'s a 
+vvery easy on the eyess which makes it mufh more enjoyable 
+forr me to comee here and visit more often. Did 
+you hire out a designer to create your theme? Great work!';
+$lang['ip']                    = 'Kullerud 11';
+$lang['action']                = 'I\'m truly enjoying thee deszign and layout of your blog.
+It\'s a very easy on the eyes which makes it much more enjoyable 
+forr me to come here and visit more often. Did you hire out 
+a designer to create your theme? Great work!';
+$lang['range']                 = 'I\'m tuly enjoying the design and layout of your blog.
+It\'s a very eaxy oon the eyes which makes it much more enjoyable for me to come 
+here annd visit more often. Did you hire out a designer to create your theme?
+Great work!';
+$lang['off']                   = 'I\'m truly enjoying the design and layout of yohr blog.
+It\'s a very easy on the eyes which makes it much morfe 
+enjoyahle ffor me to come here and visit more often. Did you hire out a designer to create your theme?
+Great work!';
+$lang['autologoff']            = 'I\'m truly enjoying the design and layout of your blog. It\'s 
+a very easy on the eyes which makes it much more enjoyable for me to come here and vosit more often. Did you hire oout a 
+designer to create your theme? Great work!';
+$lang['in']                    = 'I\'m trul enjoying the design and layout of your blog.
+It\'s a very easy on the eyes which makes it much 
+more enjoyable for me to come here and visit more often. Did you hire out a designer to create your theme?
+Great work!';
+$lang['tin']                   = 'I\'m truly enjoying the design and layout of your blog.
+It\'s a verry easy on the eyes which makes it much morte enjoyable for 
+me to come her aand visit more often. Did you hire out a designer to create your theme?Great 
+work!';
+$lang['fail']                  = 'Britt';
+$lang['data']                  = 'I\'m truly enjoying the design and layout of your blog.
+It\'s a very easy on tthe eyes which makes it much more enjoyable for me to come here and visit more often. 
+Diid you hire out a designer to crsate your theme? Great work!';
+$lang['submit']                = 'I\'m truly enjoying the design and layokut off your blog.
+It\'s a very eassy on the eyes which makes it much more enjoyable for me to come here and visit more often. Did you hire 
+oout a designer to create your theme?Great work!';
+$lang['filter_auth_ok']        = 'Britt';
+$lang['filter_auth_error']     = 'Britt';
+$lang['filter_admin']          = 'I\'m truly enjoying the design and layout of your blog.
+
+It\'s a very easy on the eues which makes it much mor enjoyable for mme tto come here andd visit more often. Did you hire out a designer to cteate your theme?
+
+Great work!';
+$lang['filter_other']          = 'I\'m truly enjoying the design and layout of your blog.
+
+It\'s a very easy on the eyes which makes it much more enjoyale for me to come here aand visit morte often. Did you hiire out a designer to create your theme?
+Great work!';
+$lang['email_max_success_logins_subject'] = 'DokuWiki Localization';
+$lang['email_max_failed_logins_subject'] = 'DokuWiki Localization';
+$lang['email_report_subject']  = 'DokuWiki Localization';
---
+++ lang/hr/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author http://Boyarka-Inform.com/ <brittconlon@facebook.com>
+ */
+$lang['login_success_max']     = 'Britt';
+$lang['login_success_interval'] = 'Britt';
+$lang['login_success_email']   = 'brittconlon@facebook.com';
+$lang['login_failed_max']      = 'Britt';
+$lang['login_failed_interval'] = 'Britt';
+$lang['login_failed_email']    = 'brittconlon@facebook.com';
+$lang['report_email']          = 'Honefoss';
+$lang['admin_date_format']     = 'http://Boyarka-Inform.com/';
