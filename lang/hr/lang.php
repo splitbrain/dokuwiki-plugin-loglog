20ac146e6bd5da5b2f98eabd1322a0cbd3532078
Skitterphoto.com <sherlyn_lira@yahoo.in>
2025-08-25 17:55:46 +0200

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -3,15 +3,45 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Skitterphoto.com <sherlyn_lira@yahoo.in>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['menu']                  = 'Tjedne prijave/odjave korisnika';
-$lang['date']                  = 'Datum';
-$lang['ip']                    = 'IP adresa';
-$lang['action']                = 'Aktivnost';
-$lang['range']                 = 'Prikazani opseg datuma:';
-$lang['off']                   = 'odjavljen';
-$lang['autologoff']            = 'bio je odjavljen od strane sustava';
-$lang['in']                    = 'prijavljen trajno';
-$lang['tin']                   = 'prijavljen trenutno';
-$lang['fail']                  = 'neuspio pokušaj prijave';
+$lang['menu']                  = 'Sherlyn';
+$lang['date']                  = 'whoah this blog is fantastic i like studying 
+your articles. Stay up the great work! You recognize, a lot of persons are searching around for this information, you 
+could help them greatly.';
+$lang['ip']                    = 'Mollelokken 66';
+$lang['action']                = 'whoah this blog is fantastic i like studying your 
+articles. Stay up the great work! You recognize, a lot of persons are searching around 
+for this information, you could help them greatly.';
+$lang['range']                 = 'whoah this blog is fantastic i like studying 
+your articles. Stay up the great work! You recognize, a lot of persons are searching around 
+for this information, you could help them greatly.';
+$lang['off']                   = 'whoah this blog is fantastic i like studying your articles.
+Stay up the great work! You recognize, a lot of persons are searching around 
+for this information, you could help them greatly.';
+$lang['autologoff']            = 'whoah this blog is fantastic i like studying your articles.
+Stay up the great work! You recognize, a lot of 
+persons are searching around for this information, you could help them greatly.';
+$lang['in']                    = 'whoah this blog is fantastic i like studying your articles.
+Stay up the great work! You recognize, a lot of persons are searching around for this information, you could help them greatly.';
+$lang['tin']                   = 'whoah this blog is fantastic i like studying your articles.
+Stay up the great work! You recognize, a lot of persons are 
+searching around for this information, you could help them greatly.';
+$lang['fail']                  = 'Sherlyn';
+$lang['data']                  = 'whoah this blog is fantastic i like studying your articles.
+Stay up the great work! You recognize, a lot of persons are searching around for this information, you 
+could help them greatly.';
+$lang['submit']                = 'whoah this blog is fantastic i like studying your articles.
+Stay up the great work! You recognize, a lot of persons are searching around 
+for this information, you could help them greatly.';
+$lang['filter_auth_ok']        = 'Sherlyn';
+$lang['filter_auth_error']     = 'Sherlyn';
+$lang['filter_admin']          = 'whoah this blog is fantastic i like studying your 
+articles. Stay up the great work! You recognize, a lot of persons are searching around for this information, you could help them greatly.';
+$lang['filter_other']          = 'whoah this blog is fantastic i like studying your articles.
+Stay up the great work! You recognize, a lot of persons are searching around for 
+this information, you could help them greatly.';
+$lang['email_max_success_logins_subject'] = 'DokuWiki Localization';
+$lang['email_max_failed_logins_subject'] = 'DokuWiki Localization';
+$lang['email_report_subject']  = 'DokuWiki Localization';
---
+++ lang/hr/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Skitterphoto.com <sherlyn_lira@yahoo.in>
+ */
+$lang['login_success_max']     = 'Sherlyn';
+$lang['login_success_interval'] = 'Sherlyn';
+$lang['login_success_email']   = 'sherlyn_lira@yahoo.in';
+$lang['login_failed_max']      = 'Sherlyn';
+$lang['login_failed_interval'] = 'Sherlyn';
+$lang['login_failed_email']    = 'sherlyn_lira@yahoo.in';
+$lang['report_email']          = 'Odense C';
+$lang['admin_date_format']     = 'https://Skitterphoto.com/photographers/885945/kline-sparks';
