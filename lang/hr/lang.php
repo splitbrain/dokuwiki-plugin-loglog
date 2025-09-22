6310ee62df7879cac6dfbe14325027b0386a2192
prozone login <cassandralestrange@gmail.com>
2025-09-22 02:55:09 +0200

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -3,15 +3,38 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author prozone login <cassandralestrange@gmail.com>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['menu']                  = 'Tjedne prijave/odjave korisnika';
-$lang['date']                  = 'Datum';
-$lang['ip']                    = 'IP adresa';
-$lang['action']                = 'Aktivnost';
-$lang['range']                 = 'Prikazani opseg datuma:';
-$lang['off']                   = 'odjavljen';
-$lang['autologoff']            = 'bio je odjavljen od strane sustava';
-$lang['in']                    = 'prijavljen trajno';
-$lang['tin']                   = 'prijavljen trenutno';
-$lang['fail']                  = 'neuspio pokušaj prijave';
+$lang['menu']                  = 'Cassandra';
+$lang['date']                  = 'Hi there to every one, the contents existing at this web site are in fact remarkable 
+for people knowledge, well, keep up the good work fellows.';
+$lang['ip']                    = 'Via Stauffacher 64';
+$lang['action']                = 'Hi there to every one, the contents existing at this web site are in fact remarkable for people knowledge, well, keep up the good 
+work fellows.';
+$lang['range']                 = 'Hi there to every one, the contents existing at this web site are in fact remarkable for people knowledge,
+well, keep up the good work fellows.';
+$lang['off']                   = 'Hi there to every one, the contents existing 
+at this web site are in fact remarkable for 
+people knowledge, well, keep up the good work fellows.';
+$lang['autologoff']            = 'Hi there to every one, the contents existing at this web site are in fact remarkable 
+for people knowledge, well, keep up the good work fellows.';
+$lang['in']                    = 'Hi there to every one, the contents existing at this web site are in fact remarkable for people knowledge, 
+well, keep up the good work fellows.';
+$lang['tin']                   = 'Hi there to every one, the contents existing at this web site are in fact remarkable for people knowledge,
+well, keep up the good work fellows.';
+$lang['fail']                  = 'Cassandra';
+$lang['data']                  = 'Hi there to every one, the contents existing at this web site are in fact remarkable for people knowledge,
+well, keep up the good work fellows.';
+$lang['submit']                = 'Hi there to every one, the contents existing at this web site are in fact remarkable 
+for people knowledge, well, keep up the good work fellows.';
+$lang['filter_auth_ok']        = 'Cassandra';
+$lang['filter_auth_error']     = 'Cassandra';
+$lang['filter_admin']          = 'Hi there to every one, the contents existing at 
+this web site are in fact remarkable for people knowledge, well, keep up the good 
+work fellows.';
+$lang['filter_other']          = 'Hi there to every one, the contents existing at this web site are in fact remarkable for people knowledge, well, 
+keep up the good work fellows.';
+$lang['email_max_success_logins_subject'] = 'DokuWiki Localization';
+$lang['email_max_failed_logins_subject'] = 'DokuWiki Localization';
+$lang['email_report_subject']  = 'DokuWiki Localization';
---
+++ lang/hr/settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author prozone login <cassandralestrange@gmail.com>
+ */
+$lang['login_success_max']     = 'Cassandra';
+$lang['login_success_interval'] = 'Cassandra';
+$lang['login_success_email']   = 'cassandralestrange@gmail.com';
+$lang['login_failed_max']      = 'Cassandra';
+$lang['login_failed_interval'] = 'Cassandra';
+$lang['login_failed_email']    = 'cassandralestrange@gmail.com';
+$lang['report_email']          = 'Villa Luganese';
+$lang['admin_date_format']     = 'https://prozonei.cc/';
